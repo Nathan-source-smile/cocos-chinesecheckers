226d936b90faeac8251a14e8e740f5fa722dcc6f
AdanBarnett <adanbarnett114@gmail.com>
2023-10-03 05:35:47 -0400

    ui 1

---
+++ assets/Script/cell.js
@@ -0,0 +1,18 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        u: 0,
+        v:0,
+        w: 0,
+    },
+
+    // use this for initialization
+    onLoad: function () {
+    },
+
+    // called every frame
+    update: function (dt) {
+
+    },
+});
